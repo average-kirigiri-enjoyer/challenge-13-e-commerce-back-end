f59e1776b98eb5be913df43f819c43f4b4875e7e
ethan <hertlethan@gmail.com>
2023-10-12 16:30:25 -0400

    added code to handle GET, POST, PUT, and DELETE requests regarding categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,7 +2,7 @@
 ethan (average-kirigiri-enjoyer)
 SCS Boot Camp Module 13 Weekly Challenge - E-Commerce Back-End
 Created 2023/10/11
-Last Edited 2023/10/11
+Last Edited 2023/10/12
 */
 
 const router = require('express').Router();
@@ -10,26 +10,101 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) =>
+{
   // find all categories
   // be sure to include its associated Products
+  try
+  {
+    const categoryData = await Category.findAll({include: [{model: Product}]});
+    res.status(200).json(categoryData);
+  }
+  catch (err) //if an internal server error occurs, responds with a status of 500 alongside the error's JSON data
+  {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) =>
+{
   // find one category by its `id` value
   // be sure to include its associated Products
+  try
+  {
+    const categoryData = await Category.findByPk(req.params.id, {include: [{model: Product}]});
+
+    //return a 404 status error if a category could not be found with the requested ID
+    if (!categoryData)
+    {
+      res.status(404).json("Could not find a category with that ID");
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  }
+  catch (err) //if an internal server error occurs, responds with a status of 500 alongside the error's JSON data
+  {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) =>
+{
   // create a new category
+  try
+  {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  }
+  catch (err) //if error occurs while trying to create a category, responds with a status of 400 alongside the error's JSON data
+  {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) =>
+{
   // update a category by its `id` value
+  try
+  {
+    const categoryData = await Category.update(req.body,
+    {
+      where: {id: req.params.id}
+    });
+
+    if (!categoryData)
+    {
+      res.status(404).json("Could not find a category with that ID");
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  }
+  catch (err) //if error occurs while trying to update a category, responds with a status of 400 alongside the error's JSON data
+  {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) =>
+{
   // delete a category by its `id` value
+  try
+  {
+    const categoryData = await Category.destroy({where: {id: req.params.id}});
+
+    if (!categoryData)
+    {
+      res.status(404).json("Could not find a category with that ID");
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  }
+  catch (err) //if error occurs while trying to delete a category, responds with a status of 500 alongside the error's JSON data
+  {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 ethan (average-kirigiri-enjoyer)
 SCS Boot Camp Module 13 Weekly Challenge - E-Commerce Back-End
 Created 2023/10/11
-Last Edited 2023/10/11
+Last Edited 2023/10/12
 */
 
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({force: false}).then(() =>
+{
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
